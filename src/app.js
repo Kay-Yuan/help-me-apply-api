2f735c0d9eaf0b20010d04f02cbf797af1996d39
jakecastelli <959672929@qq.com>
2022-02-15 12:49:41 +1030

    fix: remove console log

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/company/create", (req, res) => {
-  console.log("-----------");
-  console.log(req.body);
-  console.log("-----------");
-
   // validate body
 
   // generate uuid and add to db
