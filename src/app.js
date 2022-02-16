731a94f164ca7b697f6af9c76c116d53db1be09d
Kay-Yuan <fangyu204@gmail.com>
2022-02-16 23:18:13 +1100

    #6 feat: add job table and endpoint

--- src/app.js
+++ src/app.js
@@ -16,25 +16,29 @@ app.post("/company/create", (req, res) => {
     id: Joi.string().guid({
       version: ["uuidv4", "uuidv5"],
     }),
-
     companyName: Joi.string().required(),
-
     companyURL: Joi.string().hostname().required(),
-
     companyAddress: Joi.string(),
-
     recruiterName: Joi.string(),
-
     recruiterEmail: Joi.string().email(),
-
     recruiterNumber: Joi.string(),
   });
 
   const value = schema.validate(req.body);
-  const { companyName, companyURL, companyAddress, recruiterName, recruiterEmail, recruiterNumber } = req.body;
+  const {
+    companyName,
+    companyURL,
+    companyAddress,
+    recruiterName,
+    recruiterEmail,
+    recruiterNumber,
+  } = req.body;
   console.log(value);
 
-  if (value.error) res.send(value.error);
+  if (value.error) {
+    res.send(value.error);
+    return;
+  }
 
   // try catch?
 
@@ -60,6 +64,70 @@ app.post("/company/create", (req, res) => {
       recruiterName,
       recruiterEmail,
       recruiterNumber,
+    })
+    .then(() => {});
+
+  res.send("ok");
+});
+
+app.post("/job/create", (req, res) => {
+  // validate body
+  const schema = Joi.object({
+    id: Joi.string().guid({
+      version: ["uuidv4", "uuidv5"],
+    }),
+    companyId: Joi.number().required(),
+    jobTitle: Joi.string().required(),
+    jobLocation: Joi.string(),
+    jobDescription: Joi.string(),
+    jobRequirement: Joi.string(),
+    jobExperienceLevel: Joi.string(),
+    jobType: Joi.string(),
+    jobSalary: Joi.string(),
+  });
+
+  const value = schema.validate(req.body);
+  const {
+    companyId,
+    jobTitle,
+    jobLocation,
+    jobDescription,
+    jobRequirement,
+    jobExperienceLevel,
+    jobType,
+    jobSalary,
+  } = req.body;
+  console.log(value);
+
+  if (value.error) {
+    res.send(value.error);
+    return;
+  }
+
+  // generate uuid and add to db
+  const id = uuidv4();
+  const knex = require("knex")({
+    client: "postgres",
+    connection: {
+      host: "localhost",
+      port: 5432,
+      user: "postgres",
+      password: "postgres",
+      database: "help_me_apply",
+    },
+  });
+
+  knex("job")
+    .insert({
+      id,
+      companyId,
+      jobTitle,
+      jobLocation,
+      jobDescription,
+      jobRequirement,
+      jobExperienceLevel,
+      jobType,
+      jobSalary,
     })
     .then(() => {});
 
