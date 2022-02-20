a2338fff18cabb3e1d565490c74c42878e9658fc
jakecastelli <959672929@qq.com>
2022-02-20 22:35:02 +1030

    feat: add jobLink and jobStatus column

--- migrations/20220216110332_job_table.js
+++ migrations/20220216110332_job_table.js
@@ -6,13 +6,18 @@ exports.up = async function (knex) {
   await knex.schema.createTable("job", function (table) {
     table.uuid("id").primary();
     table.uuid("companyId").notNullable();
+    table.string("jobLink").nullable();
     table.string("jobTitle").notNullable();
     table.string("jobLocation").nullable();
     table.string("jobDescription").nullable();
     table.string("jobRequirement").nullable();
+    // junior, mid, senior
     table.string("jobExperienceLevel").nullable();
+    // part-time, full-time, contract
     table.string("jobType").nullable();
     table.string("jobSalaryRange").nullable();
+    // active, inactive
+    table.boolean("jobStatus").notNullable();
   });
   return knex.raw("GRANT ALL PRIVILEGES ON job TO postgres;");
 };
