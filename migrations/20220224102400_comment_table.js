9c08d3169f9fdbae24abf67649a15eaa37d53994
Kay-Yuan <fangyu204@gmail.com>
2022-02-25 00:11:03 +1100

    #25 #28 feat: add application table and comment table

---
+++ migrations/20220224102400_comment_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.createTable("comment", function (table) {
+    table.uuid("id").primary();
+    table.timestamp("dateCreated").notNullable();
+    table.string("content").nullable();
+    table
+      .uuid("applicationId")
+      .notNullable()
+      .references("id")
+      .inTable("application")
+      .onDelete("CASCADE");
+  });
+  return knex.raw("GRANT ALL PRIVILEGES ON company TO postgres;");
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
