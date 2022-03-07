02a76b491556997ecf1380d660b8f1272a692b07
jakecastelli <959672929@qq.com>
2022-03-07 21:25:46 +1030

    wip-34 feat: setup env file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -7,6 +8,10 @@ const companyRouter = require("./routes/company/company.route");
 const jobRouter = require("./routes/job/job.route");
 const applicationRouter = require("./routes/application/application.route");
 const commentRouter = require("./routes/comment/comment.route");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -19,4 +24,4 @@ app.use("/application", applicationRouter);
 
 app.use("/comment", commentRouter);
 
-app.listen(5000, () => console.log("Server started"));
+app.listen(PORT, () => console.log("Server started"));
