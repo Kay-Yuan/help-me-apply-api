08e772e1dbc89a51309f05de715435ed971d22fa
Kay-Yuan <fangyu204@gmail.com>
2022-03-11 10:52:24 +1030

    test

--- src/routes/company/company.route.js
+++ src/routes/company/company.route.js
@@ -5,164 +5,164 @@ const knex = require("../../config/db");
 import logger from "../../logger";
 
 const {
-	createCompanySchema,
-	getCompanySchema,
-	getCompaniesSchema,
-	deleteCompanySchema,
-	updateCompanySchema,
-	searchCompanySchema,
+  createCompanySchema,
+  getCompanySchema,
+  getCompaniesSchema,
+  deleteCompanySchema,
+  updateCompanySchema,
+  searchCompanySchema,
 } = require("../../schema/company.schema");
 
 router.post("/create", createCompanySchema, (req, res) => {
-	const {
-		companyName,
-		companyURL,
-		companyAddress,
-		recruiterName,
-		recruiterEmail,
-		recruiterNumber,
-		rate,
-	} = req.body;
+  const {
+    companyName,
+    companyURL,
+    companyAddress,
+    recruiterName,
+    recruiterEmail,
+    recruiterNumber,
+    rate,
+  } = req.body;
 
-	// generate uuid and add to db
-	const id = uuidv4();
+  // generate uuid and add to db
+  const id = uuidv4();
 
-	knex("company")
-		.insert({
-			id,
-			companyName,
-			companyURL,
-			companyAddress,
-			recruiterName,
-			recruiterEmail,
-			recruiterNumber,
-			rate,
-		})
-		.then(() => {
-			logger.info("company created");
+  knex("company")
+    .insert({
+      id,
+      companyName,
+      companyURL,
+      companyAddress,
+      recruiterName,
+      recruiterEmail,
+      recruiterNumber,
+      rate,
+    })
+    .then(() => {
+      logger.info("company created");
 
-			res.send({ message: "company created" });
-		})
-		.catch((e) => {
-			logger.error(e);
+      res.send({ message: "company created" });
+    })
+    .catch((e) => {
+      logger.error(e);
 
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 });
 
 router.get("/search", searchCompanySchema, (req, res) => {
-	const search = req.query.name;
+  const search = req.query.name;
 
-	knex("company")
-		.where("companyName", "ilike", `%${search}%`)
-		.then((queryResult) => {
-			console.log(queryResult);
+  knex("company")
+    .where("companyName", "ilike", `%${search}%`)
+    .then((queryResult) => {
+      console.log(queryResult);
 
-			if (queryResult.length) {
-				res.send(queryResult.slice(0, 5));
-			} else {
-				res.send({ message: "company not found" });
-			}
-		})
-		.catch(() => {
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+      if (queryResult.length) {
+        res.send(queryResult.slice(0, 5));
+      } else {
+        res.send({ message: "company not found" });
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 });
 
 router.get("/:companyId", getCompanySchema, (req, res) => {
-	const companyId = req.params.companyId;
+  const companyId = req.params.companyId;
 
-	knex("company")
-		.where({ id: companyId })
-		.then((queryResult) => {
-			const company = queryResult[0];
+  knex("company")
+    .where({ id: companyId })
+    .then((queryResult) => {
+      const company = queryResult[0];
 
-			if (company) {
-				res.send(company);
-			} else {
-				res.status(400);
-				res.send({ message: "company not found" });
-			}
-		})
-		.catch(() => {
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+      if (company) {
+        res.send(company);
+      } else {
+        res.status(400);
+        res.send({ message: "company not found" });
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 });
 
 router.get("/", getCompaniesSchema, (req, res) => {
-	// get query out from req
-	if (!req.query.offset) {
-		res.status(400);
-		res.send("/company request need a query for offset!");
-		return;
-	}
-	const offset = req.query.offset;
-	const limit = 10;
+  // get query out from req
+  if (!req.query.offset) {
+    res.status(400);
+    res.send("/company request need a query for offset!");
+    return;
+  }
+  const offset = req.query.offset;
+  const limit = 10;
 
-	// get result from db
-	knex("company")
-		.limit(limit)
-		.offset(offset)
-		.then((companyList) => {
-			res.send(companyList);
-		})
-		.catch((e) => {
-			logger.error(e);
+  // get result from db
+  knex("company")
+    .limit(limit)
+    .offset(offset)
+    .then((companyList) => {
+      res.send(companyList);
+    })
+    .catch((e) => {
+      logger.error(e);
 
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 });
 
 router.delete("/:companyId", deleteCompanySchema, (req, res) => {
-	const companyId = req.params.companyId;
+  const companyId = req.params.companyId;
 
-	knex("company")
-		.where({ id: companyId })
-		.del()
-		.then(() => {})
-		.catch(() => {
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+  knex("company")
+    .where({ id: companyId })
+    .del()
+    .then(() => {})
+    .catch(() => {
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 
-	res.send({ message: "company deleted" });
+  res.send({ message: "company deleted" });
 });
 
 router.put("/:companyId", updateCompanySchema, (req, res) => {
-	const companyId = req.params.companyId;
+  const companyId = req.params.companyId;
 
-	const {
-		companyName,
-		companyURL,
-		companyAddress,
-		recruiterName,
-		recruiterEmail,
-		recruiterNumber,
-		rate,
-	} = req.body;
+  const {
+    companyName,
+    companyURL,
+    companyAddress,
+    recruiterName,
+    recruiterEmail,
+    recruiterNumber,
+    rate,
+  } = req.body;
 
-	knex("company")
-		.where({ id: companyId })
-		.update({
-			companyName,
-			companyURL,
-			companyAddress,
-			recruiterName,
-			recruiterEmail,
-			recruiterNumber,
-			rate,
-		})
-		.then(() => {})
-		.catch(() => {
-			res.status(500);
-			res.send("INTERNAL SERVER ERROR");
-		});
+  knex("company")
+    .where({ id: companyId })
+    .update({
+      companyName,
+      companyURL,
+      companyAddress,
+      recruiterName,
+      recruiterEmail,
+      recruiterNumber,
+      rate,
+    })
+    .then(() => {})
+    .catch(() => {
+      res.status(500);
+      res.send("INTERNAL SERVER ERROR");
+    });
 
-	res.send({ message: "company updated" });
+  res.send({ message: "company updated" });
 });
 
 module.exports = router;
